fc71844604e71d344477ac7766db34c32062fbb0
hikio-17 <fajri010217@gmail.com>
2023-05-08 06:56:26 -0700

    update

--- module/biodata.js
+++ module/biodata.js
@@ -1,9 +1,6 @@
 const getName = (name) => name;
-
 const getBirth = (birth) => birth;
-
 const getAddress = (address) => address;
-
 const getPlaceBirth = (placeBirth) => placeBirth;
 
 module.exports = {
